99a32d3223d06fe04897680a29435c0c9cdf8fa8
grey4uk <grey4uk14@gmail.com>
2022-04-06 14:01:29 +0300

    update: added jsconfig.json for absolute import

--- bc14online-components/src/App.js
+++ bc14online-components/src/App.js
@@ -1,4 +1,4 @@
-import { Component} from 'react';
+import { Component } from 'react';
 import Site from './components/Site/Site';
 import Title from './components/Title/Title';
 import Card from './components/Card/Card';
@@ -18,23 +18,23 @@ import UserList from './db/userList.json';
 
 // export default App;
 class App extends Component {
-        title='Hello friends!!!'
+  title = 'Hello friends!!!';
 
-        render() {
-                return (
-                        <>
-                         <Site/>
-                         <Section>
-                                 <Title />
-                                 {/* <Title title={this.title}/> */}
-                                 {/* <Card /> */}
-                                 <Card source='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*'/>
-                         </Section>
-                         {/* <ElementsList elements={[]}/> */}
-                         <ElementsList elements={UserList}/>
-                        </>
-                );
-        }
+  render() {
+    return (
+      <>
+        <Site />
+        <Section>
+          <Title />
+          {/* <Title title={this.title}/> */}
+          {/* <Card /> */}
+          <Card source='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*' />
+        </Section>
+        {/* <ElementsList elements={[]}/> */}
+        <ElementsList elements={UserList} />
+      </>
+    );
+  }
 }
 
 export default App;
